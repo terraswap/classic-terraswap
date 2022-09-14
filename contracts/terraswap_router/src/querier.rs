f575f8e816fa551b4328f2e0acbbe9b001a71945
JoowonYun <joowon@delightlabs.io>
2022-09-14 20:09:00 +0900

    feat: tax on luna

--- contracts/terraswap_router/src/querier.rs
+++ contracts/terraswap_router/src/querier.rs
@@ -6,10 +6,6 @@ use terra_cosmwasm::TerraQuerier;
 static DECIMAL_FRACTION: Uint128 = Uint128::new(1_000_000_000_000_000_000u128);
 
 pub fn compute_tax(querier: &QuerierWrapper, amount: Uint128, denom: String) -> StdResult<Uint128> {
-    if denom == "uluna" {
-        return Ok(Uint128::zero());
-    }
-
     let terra_querier = TerraQuerier::new(querier);
     let tax_rate: Decimal = (terra_querier.query_tax_rate()?).rate;
     let tax_cap: Uint128 = (terra_querier.query_tax_cap(denom)?).cap;
@@ -27,10 +23,6 @@ pub fn compute_reverse_tax(
     amount: Uint128,
     denom: String,
 ) -> StdResult<Uint128> {
-    if denom == "uluna" {
-        return Ok(Uint128::zero());
-    }
-
     let terra_querier = TerraQuerier::new(querier);
     let tax_rate: Decimal = (terra_querier.query_tax_rate()?).rate;
     let tax_cap: Uint128 = (terra_querier.query_tax_cap(denom)?).cap;
