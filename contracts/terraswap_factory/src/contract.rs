terraswap/terraswap

refactor: PairInfo saved decimal & validation - remove legacy & stargate

contracts/terraswap_factory/src/contract.rs
@@ -1,8 +1,8 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Addr, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Reply, ReplyOn, Response,
-    StdError, StdResult, SubMsg, WasmMsg,
+    to_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Empty, Env, MessageInfo, Reply, ReplyOn,
+    Response, StdError, StdResult, SubMsg, WasmMsg,
 };
 
 use crate::querier::query_liquidity_token;
@@ -90,7 +90,7 @@ pub fn execute_update_config(
 // Anyone can execute it to create swap pair
 pub fn execute_create_pair(
     deps: DepsMut<Empty>,
-    _env: Env,
+    env: Env,
     _info: MessageInfo,
     asset_infos: [AssetInfo; 2],
 ) -> StdResult<Response> {
@@ -100,14 +100,24 @@ pub fn execute_create_pair(
         return Err(StdError::generic_err("same asset"));
     }
 
-    if !(asset_infos[0].is_valid(&deps.querier) && asset_infos[1].is_valid(&deps.querier)) {
-        return Err(StdError::generic_err("invalid asset"));
-    }
+    let asset_1_decimal =
+        match asset_infos[0].query_decimals(env.contract.address.clone(), &deps.querier) {
+            Ok(decimal) => decimal,
+            Err(_) => return Err(StdError::generic_err("asset1 is invalid")),
+        };
+
+    let asset_2_decimal =
+        match asset_infos[1].query_decimals(env.contract.address.clone(), &deps.querier) {
+            Ok(decimal) => decimal,
+            Err(_) => return Err(StdError::generic_err("asset2 is invalid")),
+        };
 
     let raw_infos = [
         asset_infos[0].to_raw(deps.api)?,
         asset_infos[1].to_raw(deps.api)?,
     ];
+
+    let asset_decimals = [asset_1_decimal, asset_2_decimal];
 
     let pair_key = pair_key(&raw_infos);
     if let Ok(Some(_)) = PAIRS.may_load(deps.storage, &pair_key) {
@@ -119,6 +129,7 @@ pub fn execute_create_pair(
         &TmpPairInfo {
             pair_key,
             asset_infos: raw_infos,
+            asset_decimals,
         },
     )?;
 
@@ -130,17 +141,17 @@ pub fn execute_create_pair(
         .add_submessage(SubMsg {
             id: 1,
             gas_limit: None,
-            msg: WasmMsg::Instantiate {
+            msg: CosmosMsg::Wasm(WasmMsg::Instantiate {
                 code_id: config.pair_code_id,
                 funds: vec![],
-                admin: None,
+                admin: Some(env.contract.address.to_string()),
                 label: "pair".to_string(),
                 msg: to_binary(&PairInstantiateMsg {
                     asset_infos,
                     token_code_id: config.token_code_id,
+                    asset_decimals,
                 })?,
-            }
-            .into(),
+            }),
             reply_on: ReplyOn::Success,
         }))
 }
@@ -165,6 +176,7 @@ pub fn reply(deps: DepsMut<Empty>, _env: Env, msg: Reply) -> StdResult<Response>
             liquidity_token: deps.api.addr_canonicalize(liquidity_token.as_str())?,
             contract_addr: deps.api.addr_canonicalize(pair_contract)?,
             asset_infos: tmp_pair_info.asset_infos,
+            asset_decimals: tmp_pair_info.asset_decimals,
         },
     )?;
 

terraswap/classic-terraswap

contracts/terraswap_factory/src/contract.rs
@@ -1,8 +1,8 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Reply, ReplyOn, Response, StdError,
-    StdResult, SubMsg, WasmMsg,
+    to_binary, Addr, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Reply, ReplyOn, Response,
+    StdError, StdResult, SubMsg, WasmMsg,
 };
 
 use crate::querier::query_liquidity_token;
@@ -85,7 +85,7 @@ pub fn execute_update_config(
 // Anyone can execute it to create swap pair
 pub fn execute_create_pair(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     _info: MessageInfo,
     asset_infos: [AssetInfo; 2],
 ) -> StdResult<Response> {
@@ -95,14 +95,24 @@ pub fn execute_create_pair(
         return Err(StdError::generic_err("same asset"));
     }
 
-    if !(asset_infos[0].is_valid(&deps.querier) && asset_infos[1].is_valid(&deps.querier)) {
-        return Err(StdError::generic_err("invalid asset"));
-    }
+    let asset_1_decimal =
+        match asset_infos[0].query_decimals(env.contract.address.clone(), &deps.querier) {
+            Ok(decimal) => decimal,
+            Err(_) => return Err(StdError::generic_err("asset1 is invalid")),
+        };
+
+    let asset_2_decimal =
+        match asset_infos[1].query_decimals(env.contract.address.clone(), &deps.querier) {
+            Ok(decimal) => decimal,
+            Err(_) => return Err(StdError::generic_err("asset2 is invalid")),
+        };
 
     let raw_infos = [
         asset_infos[0].to_raw(deps.api)?,
         asset_infos[1].to_raw(deps.api)?,
     ];
+
+    let asset_decimals = [asset_1_decimal, asset_2_decimal];
 
     let pair_key = pair_key(&raw_infos);
     if let Ok(Some(_)) = PAIRS.may_load(deps.storage, &pair_key) {
@@ -114,6 +124,7 @@ pub fn execute_create_pair(
         &TmpPairInfo {
             pair_key,
             asset_infos: raw_infos,
+            asset_decimals,
         },
     )?;
 
@@ -125,17 +136,17 @@ pub fn execute_create_pair(
         .add_submessage(SubMsg {
             id: 1,
             gas_limit: None,
-            msg: WasmMsg::Instantiate {
+            msg: CosmosMsg::Wasm(WasmMsg::Instantiate {
                 code_id: config.pair_code_id,
                 funds: vec![],
-                admin: None,
-                label: "".to_string(),
+                admin: Some(env.contract.address.to_string()),
+                label: "pair".to_string(),
                 msg: to_binary(&PairInstantiateMsg {
                     asset_infos,
                     token_code_id: config.token_code_id,
+                    asset_decimals,
                 })?,
-            }
-            .into(),
+            }),
             reply_on: ReplyOn::Success,
         }))
 }
@@ -160,6 +171,7 @@ pub fn reply(deps: DepsMut, _env: Env, msg: Reply) -> StdResult<Response> {
             liquidity_token: deps.api.addr_canonicalize(liquidity_token.as_str())?,
             contract_addr: deps.api.addr_canonicalize(pair_contract)?,
             asset_infos: tmp_pair_info.asset_infos,
+            asset_decimals: tmp_pair_info.asset_decimals,
         },
     )?;
 